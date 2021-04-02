7a57e0466422d4f6f96d16ae3a5ddfc3fddeddef
goksenpasli <gpasli@gmail.com>
2021-04-02 22:56:02 +0300

    Add project files.

---
+++ BesinPiramit/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace BesinPiramit
+{
+   public class MainViewModel
+    {
+
+        public MainViewModel() => Show = new RelayCommand<object>(parameter => (parameter as Path).Fill = Brushes.Transparent, parameter => true);
+
+        public ICommand Show { get; }
+
+    }
+}
